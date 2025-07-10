4ab8982bb4ce050b6145c10faefadf534aae90d8
Tejo Priya Boddu <boddutejopriya@gmail.com>
2025-07-10 21:49:03 +0530

    Create odd_parity_generator.v

---
+++ odd_parity_generator.v
@@ -0,0 +1,10 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module odd_parity(
+input [3:0]in,
+output parity
+    );
+    assign parity = ~(in[3]^in[2]^in[1])^in[0];
+endmodule
